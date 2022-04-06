ccfeab9a7818148dc2c2a816cb2affa8abf23c0f
dvora-h <67596500+dvora-h@users.noreply.github.com>
2022-04-06 12:20:09 +0300

    add deprecation warning

---
+++ redisbloom/__init__.py
@@ -0,0 +1,4 @@
+from warnings import warn
+warn("Please upgrade to redis-py (https://pypi.org/project/redis/) "
+"This library is deprecated, and all features have been merged into redis-py.",
+DeprecationWarning, stacklevel=2)
